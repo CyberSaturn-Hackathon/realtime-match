facb7c3eeae40b389dcf9517c1f198ed05679843
iamthepoe <lucagamerbrime@gmail.com>
2023-06-21 14:03:29 -0300

    create method

--- models/DatabaseClient.js
+++ models/DatabaseClient.js
@@ -1,5 +1,32 @@
-class DatabaseClient {
-	constructor(database) {
-		this.database = database;
+class MatchModel {
+	#db;
+	constructor(databaseClient) {
+		this.#db = databaseClient;
+	}
+
+	async create(data) {
+		try {
+			const {
+				teamA,
+				teamB,
+				pointsA,
+				pointsB,
+				tournament,
+				place,
+				modality,
+			} = data;
+
+			await this.#db('matchs').insert({
+				teamA,
+				teamB,
+				pointsA,
+				pointsB,
+				tournament,
+				place,
+				modality,
+			});
+		} catch (e) {
+			throw e;
+		}
 	}
 }
