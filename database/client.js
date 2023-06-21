72a9e50af3857474950239db72448985a65f71ab
iamthepoe <lucagamerbrime@gmail.com>
2023-06-21 13:53:42 -0300

    database

---
+++ database/client.js
@@ -0,0 +1,26 @@
+const knex = require('knex')({
+	client: 'better-sqlite3',
+	connection: {
+		filename: './database.sqlite',
+	},
+});
+
+knex.schema
+	.createTableIfNotExists('match', function (table) {
+		table.increments('id').primary();
+		table.string('teamA');
+		table.string('teamB');
+		table.integer('pointsA');
+		table.integer('pointsB');
+		table.string('tournament');
+		table.string('place');
+		table.string('modality');
+		table.timestamp('created_at').defaultTo(knex.fn.now());
+		table.timestamp('updated_at').defaultTo(knex.fn.now());
+	})
+	.then(() => {
+		knex.destroy();
+	})
+	.catch((err) => {
+		console.error(err);
+	});
