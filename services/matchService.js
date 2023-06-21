97eac63d213bea2d090ea2268e41dccab1bb916e
iamthepoe <lucagamerbrime@gmail.com>
2023-06-21 14:24:45 -0300

    create method

---
+++ services/matchService.js
@@ -0,0 +1,38 @@
+class MatchService {
+	#repository;
+	constructor(matchRepository) {
+		this.#repository = matchRepository;
+	}
+
+	async create(data) {
+		try {
+			const { teamA, teamB, tournament, place, modality } = data;
+
+			if (
+				!teamA.trim() ||
+				!teamB.trim() ||
+				!tournament.trim() ||
+				!place.trim() ||
+				!modality.trim()
+			) {
+				return {
+					code: 400,
+					message: 'Todos os campos são obrigatórios!',
+					data: { error: true },
+				};
+			}
+
+			const response = await this.#repository.create({
+				...data,
+				pointsA: 0,
+				pointsB: 0,
+			});
+
+			return {
+				code: 201,
+				message: 'Registro de partida criado com sucesso.',
+				data: { ...response },
+			};
+		} catch {}
+	}
+}
