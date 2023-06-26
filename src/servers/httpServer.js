12d368124661a98fd69c3c9e3e5fce988a772b38
iamthepoe <lucagamerbrime@gmail.com>
2023-06-26 14:22:59 -0300

    create httpServer.js

---
+++ src/servers/httpServer.js
@@ -0,0 +1,41 @@
+import express from 'express';
+import { createMatchService } from './factories/matchServiceFactory.js';
+
+const matchService = createMatchService();
+
+export function configureHTTPServer(app) {
+  app.set('view engine', 'ejs');
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: false }));
+
+  app.get('/', (_, res) => {
+    return res.render('index');
+  });
+
+  app.get('/create/match', (_, res) => {
+    return res.render('admin/createMatch');
+  });
+
+  app.post('/save/match', async (req, res) => {
+    const { teamA, teamB, tournament, place, modality } = req.body;
+    const result = await matchService.create({
+      teamA,
+      teamB,
+      tournament,
+      place,
+      modality,
+    });
+
+    const { data, message, code } = result;
+
+    return res.status(code).json({ message, data });
+  });
+
+  app.get('/adm', (_, res) => {
+    return res.render('admin');
+  });
+
+  app.get('/session/points', (_, res) => {
+    return res.json(SESSION_POINTS_DATA);
+  });
+}
