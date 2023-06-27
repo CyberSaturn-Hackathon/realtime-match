10704ff8d1da44a2702204bf8891f0c7c7ce53c5
iamthepoe <lucagamerbrime@gmail.com>
2023-06-27 10:24:30 -0300

    findAll

--- src/services/pointsDataService.js
+++ src/services/pointsDataService.js
@@ -3,4 +3,16 @@ export class pointsDataService {
 	constructor(pointsDataRepository) {
 		this.#repository = pointsDataRepository;
 	}
+
+	async findAll() {
+		try {
+			const response = await this.#repository.findAll();
+			return {
+				data: { ...response },
+				message: 'Pontos encontrados',
+			};
+		} catch (e) {
+			return { error: true, content: e };
+		}
+	}
 }
