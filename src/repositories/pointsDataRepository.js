828eb824efb53f54ffbaf785b2b30574600bdee2
iamthepoe <lucagamerbrime@gmail.com>
2023-06-27 10:17:11 -0300

    findAll

--- src/repositories/pointsDataRepository.js
+++ src/repositories/pointsDataRepository.js
@@ -1,16 +1,27 @@
 import fs from 'fs';
 import path from 'path';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
 
-export class PointsDataRepository{
-    #fs;
-    #filePath = path.join(__dirname, '../../database/points/pointsData.json');
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
-    constructor(){
-        this.#fs = fs;
-    }
+export class PointsDataRepository {
+	#fs;
+	#filePath = path.join(__dirname, '../../database/points/pointsData.json');
 
-    async findAll(){
-        data = await this.#fs.readFile(this.#filePath, { encoding: 'utf-8' });
-        return data;
-    }
+	constructor() {
+		this.#fs = fs.promises;
+	}
+
+	async findAll() {
+		try {
+			const data = await this.#fs.readFile(this.#filePath, {
+				encoding: 'utf-8',
+			});
+
+			return data;
+		} catch (e) {
+			throw e;
+		}
+	}
 }
