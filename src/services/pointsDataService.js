2cc6c0a14b8c8c6ad48e65783429096c302967e0
iamthepoe <lucagamerbrime@gmail.com>
2023-06-27 11:36:32 -0300

    add code status

--- src/services/pointsDataService.js
+++ src/services/pointsDataService.js
@@ -10,26 +10,34 @@ export class PointsDataService {
 			return {
 				data: { ...response },
 				message: 'Pontos encontrados',
+				code: 200,
 			};
 		} catch (e) {
-			return { error: true, content: e };
+			return { error: true, content: e, code: 500 };
 		}
 	}
 
 	async findOne(id) {
 		try {
-			if (isNaN(id)) return { error: true, content: 'Value is NaN' };
+			if (isNaN(id))
+				return {
+					error: true,
+					content: 'Value is not a number',
+					code: 400,
+				};
 
 			const response = await this.#repository.findOne(id);
 
-			if (!response) return { error: true, content: 'Not found.' };
+			if (!response)
+				return { error: true, content: 'Not found.', code: 404 };
 
 			return {
 				data: { ...response },
 				message: 'Ponto encontrado',
+				code: 200,
 			};
 		} catch (e) {
-			return { error: true, content: e };
+			return { error: true, content: e, code: 500 };
 		}
 	}
 }
