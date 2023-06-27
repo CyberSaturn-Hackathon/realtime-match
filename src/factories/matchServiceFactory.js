aa686edb012d48f7d575e9e395abbc1d0ba44850
iamthepoe <lucagamerbrime@gmail.com>
2023-06-27 10:25:32 -0300

    fix path

--- src/factories/matchServiceFactory.js
+++ src/factories/matchServiceFactory.js
@@ -1,11 +1,11 @@
 import { client } from '../../database/client.js';
-import { MatchRepository } from '../repositories/matchRepository.js';
+import { MatchRepository } from '../repositories/MatchRepository.js';
 import { MatchService } from '../services/matchService.js';
 
 const createMatchService = () => {
-  const matchRepository = new MatchRepository(client);
-  const matchService = new MatchService(matchRepository);
-  return matchService;
+	const matchRepository = new MatchRepository(client);
+	const matchService = new MatchService(matchRepository);
+	return matchService;
 };
 
 export { createMatchService };
