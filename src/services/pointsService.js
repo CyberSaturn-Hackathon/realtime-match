466ad91cf5a9ea4fdf778fefa26a19f136c78032
iamthepoe <luca1901p@gmail.com>
2023-06-29 23:52:42 -0300

    refactoring

--- src/services/pointsService.js
+++ src/services/pointsService.js
@@ -1,67 +1,83 @@
 export class PointsService {
-	#repository;
-	constructor(pointsDataRepository) {
-		this.#repository = pointsDataRepository;
-	}
+  #repository;
+  constructor(pointsDataRepository) {
+    this.#repository = pointsDataRepository;
+  }
 
-	async findAll() {
-		try {
-			const response = await this.#repository.findAll();
-			return {
-				data: { ...response },
-				message: 'Pontos encontrados',
-				code: 200,
-			};
-		} catch (e) {
-			return { error: true, content: e, code: 500 };
-		}
-	}
+  async findAll() {
+    try {
+      const response = await this.#repository.findAll();
+      return {
+        data: { ...response },
+        message: 'Pontos encontrados',
+        code: 200,
+      };
+    } catch (e) {
+      return {
+        code: 500,
+        message: 'Erro interno!',
+        data: { error: true, content: String(e) }
+      };
+    }
+  }
 
-	async findOne(id) {
-		try {
-			if (isNaN(id))
-				return {
-					error: true,
-					content: 'Value is not a number',
-					code: 400,
-				};
+  async findOne(id) {
+    try {
+      if (isNaN(id))
+        return {
+          error: true,
+          content: 'Value is not a number',
+          code: 400,
+        };
 
-			const response = await this.#repository.findOne(id);
+      const response = await this.#repository.findOne(id);
 
-			if (!response)
-				return { error: true, content: 'Not found.', code: 404 };
+      if (!response)
+        return { error: true, content: 'Not found.', code: 404 };
 
-			return {
-				data: { ...response },
-				message: 'Ponto encontrado',
-				code: 200,
-			};
-		} catch (e) {
-			return { error: true, content: e, code: 500 };
-		}
-	}
+      return {
+        data: { ...response },
+        message: 'Ponto encontrado',
+        code: 200,
+      };
+    } catch (e) {
+      return {
+        code: 500,
+        message: 'Erro interno!',
+        data: { error: true, content: String(e) }
+      };
+    }
+  }
 
-	async create(id, pointsA, pointsB) {
-		try {
-			if (isNaN(id))
-				return {
-					error: true,
-					content: 'Id is not a number',
-					code: 400,
-				};
+  async create(id, pointsA, pointsB) {
+    try {
+      if (isNaN(id))
+        return {
+          data: {
+            error: true,
+            content: 'Id is not a number',
+          },
+          message: 'Id is not a number',
+          code: 400,
+        };
 
-			const response = await this.#repository.create(id, {
-				pointsA,
-				pointsB,
-			});
+      const response = await this.#repository.create(id, {
+        pointsA,
+        pointsB,
+      });
 
-			return {
-				data: response,
-				message: 'Pontos criados',
-				code: 201,
-			};
-		} catch (e) {
-			return { error: true, content: e, code: 500 };
-		}
-	}
+      return {
+        data: { ...response },
+        message: 'Pontos criados',
+        code: 201,
+      };
+
+    } catch (e) {
+      return {
+        code: 500,
+        message: 'Erro interno!',
+        data: { error: true, content: String(e) }
+      };
+    }
+  }
 }
