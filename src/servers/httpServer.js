69d717250d841f616ba21bc87eb90e4474716070
iamthepoe <luca1901p@gmail.com>
2023-06-30 15:55:08 -0300

    add route

--- src/servers/httpServer.js
+++ src/servers/httpServer.js
@@ -6,42 +6,50 @@ const pointsService = createPointsService();
 const matchService = createMatchService();
 
 const configureHTTPServer = (app) => {
-	app.set('view engine', 'ejs');
-	app.use(express.json());
-	app.use(express.urlencoded({ extended: false }));
+  app.set('view engine', 'ejs');
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: false }));
 
-	app.get('/', (_, res) => {
-		return res.render('index');
-	});
+  app.get('/', (_, res) => {
+    return res.render('index');
+  });
 
-	app.get('/create/match', (_, res) => {
-		return res.render('admin/createMatch');
-	});
+  app.get('/create/match', (_, res) => {
+    return res.render('admin/createMatch');
+  });
 
-	app.post('/save/match', async (req, res) => {
-		const { teamA, teamB, tournament, place, modality } = req.body;
-		const result = await matchService.create({
-			teamA,
-			teamB,
-			tournament,
-			place,
-			modality,
-		});
+  app.post('/save/match', async (req, res) => {
+    const { teamA, teamB, tournament, place, modality } = req.body;
+    const result = await matchService.create({
+      teamA,
+      teamB,
+      tournament,
+      place,
+      modality,
+    });
 
-		const { data, message, code } = result;
+    const { data, message, code } = result;
 
-		return res.status(code).json({ message, data });
-	});
+    return res.status(code).json({ message, data });
+  });
 
-	app.get('/adm', (_, res) => {
-		return res.render('admin');
-	});
+  app.post('/update/match/:id', async (req, res) => {
+    const id = req.params.id;
 
-	app.get('/session/points/:id', async (req, res) => {
-		const result = await pointsService.findOne(req.params.id);
-		const { code, data } = result;
-		return res.status(code).json(data);
-	});
+    const result = await matchService.update(id, { ...req.body });
+    const { code, data } = result;
+    return res.status(code).json(data);
+  });
+
+  app.get('/adm', (_, res) => {
+    return res.render('admin');
+  });
+
+  app.get('/session/points/:id', async (req, res) => {
+    const result = await pointsService.findOne(req.params.id);
+    const { code, data } = result;
+    return res.status(code).json(data);
+  });
 };
 
 export { configureHTTPServer };
